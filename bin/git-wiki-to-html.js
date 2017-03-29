2173cceeffd6071b55fcd5a43e01702336125fd7
Adrian Tanasa <adrian_tanasa@yahoo.com>
2017-03-29 16:09:21 +0100

    [TASK 109477] Make the template for menus customizable

--- bin/git-wiki-to-html.js
+++ bin/git-wiki-to-html.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 /**
  * CLI tool for transforming markdown files to html from a src folder to dest
- * usage: node ./git-wiki-to-html.js [srcFolder] [dstFolder] [rules-file-or-json]
+ * usage: node ./git-wiki-to-html.js [srcFolder] [dstFolder] [rules-json-file] [menu-tpl-json-file]
+ *
+ * rules/menu files defaults to ./data/tpl/*-ibm-northstar.json ones if not provided
  *
  * Note: After `npm install` the path to ./node_modules/bin/ can be used
  *
@@ -19,7 +21,8 @@ if (process.argv.length < 3 || process.argv[2] == '--help') {
     process.exit(0);
 }
 
-var rulesTplFile = '../data/tpl/ibm-northstar.json';
+var rulesTplFile = '../data/tpl/rules-ibm-northstar.json';
+var menuTplFile = '../data/tpl/menu-ibm-northstar.json';
 
 if (process.argv[4] && process.argv[4].match(/\.json$/)) {
     rulesTplFile = path.isAbsolute(process.argv[4]) ? process.argv[4] : path.join(process.cwd(), process.argv[4]);
@@ -35,10 +38,29 @@ try {
     process.exit(1);
 }
 
+if (process.argv[5] && process.argv[5].match(/\.json$/)) {
+    menuTplFile = path.isAbsolute(process.argv[5]) ? process.argv[5] : path.join(process.cwd(), process.argv[5]);
+}
+
+myConsole.log('Using menu tpl from: %s', menuTplFile);
+
+var menuTpls = {};
+try {
+    menuTpls = require(menuTplFile);
+} catch (err) {
+    myConsole.log('Failed to load menu tpls from JSON file: ', err);
+    process.exit(1);
+}
+
+if (process.argv[5] && process.argv[5].match(/\.json$/)) {
+    menuTplFile = path.isAbsolute(process.argv[5]) ? process.argv[5] : path.join(process.cwd(), process.argv[5]);
+}
+
 var obj = new GitWikiToHTML({
     'srcDir': process.argv[2] || null,
     'destDir': process.argv[3] || null,
-    'rules': rules
+    'rules': rules,
+    'menuTpls': menuTpls
 });
 
 obj.transform().then(function() {
