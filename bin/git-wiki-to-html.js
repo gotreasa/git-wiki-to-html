062e73447f3e45607127de676f8a79ef0541141d
Adrian Tanasa <adrian_tanasa@yahoo.com>
2017-05-10 15:45:19 +0100

    [TASK 111774] Use rules/templates for help generator from MySa in order to easily customize them

--- bin/git-wiki-to-html.js
+++ bin/git-wiki-to-html.js
@@ -3,56 +3,56 @@
  * CLI tool for transforming markdown files to html from a src folder to dest
  * usage: node ./git-wiki-to-html.js [srcFolder] [dstFolder] [rules-json-file] [menu-tpl-json-file]
  *
- * rules/menu files defaults to ./data/tpl/*-ibm-northstar.json ones if not provided
+ * rules/menu files defaults to ./data/default/*  - if not provided
+ * new rules/menu items are merged on top of default rules
  *
  * Note: After `npm install` the path to ./node_modules/bin/ can be used
  *
  */
-
-var path = require('path');
-var GitWikiToHTML = require('../');
+'use strict';
+const path = require('path');
+const GitWikiToHTML = require('../');
 const Console = require('console').Console;
+const _ = require('lodash');
 
-var myConsole = new Console(process.stdout, process.stderr);
+const myConsole = new Console(process.stdout, process.stderr);
 if (process.argv.length < 3 || process.argv[2] == '--help') {
     myConsole.log(
-        'usage: node ./git-wiki-to-html.js [srcFolder] [dstFolder] [TODO:rules-file-or-json]'
+        'usage: node ./git-wiki-to-html.js [srcFolder] [dstFolder] [custom-rules-folder]'
     );
     process.exit(0);
 }
 
-var rulesTplFile = '../data/tpl/rules-ibm-northstar.json';
-var menuTplFile = '../data/tpl/menu-ibm-northstar.json';
+// load default rules
+let rules = require('../data/default/rules.json');
+let menuTpls = require('../data/default/menu.json');
 
-if (process.argv[4] && process.argv[4].match(/\.json$/)) {
-    rulesTplFile = path.isAbsolute(process.argv[4]) ? process.argv[4] : path.join(process.cwd(), process.argv[4]);
+// overrite rules/menu tpl if any folder required
+if (process.argv[4]) {
+    let templatesFolder = path.isAbsolute(process.argv[4]) ? process.argv[4] :
+        path.join(process.cwd(), process.argv[4]);
+    let concatArrays = function(objValue, srcValue) {
+        if (_.isArray(objValue)) {
+            return objValue.concat(srcValue);
+        }
+    };
+    myConsole.log('Merging additional rules/menu from: %s', templatesFolder);
+    try {
+        let additionalMenuTpls = require(path.join(templatesFolder, '/menu.json'));
+        menuTpls = _.mergeWith(menuTpls, additionalMenuTpls, concatArrays);
+    } catch (err) {
+        myConsole.log('No additional menu templates present in provided templates folder', err);
+    }
+
+    try {
+        let additionalRules = require(path.join(templatesFolder, '/rules.json'));
+        rules = _.mergeWith(rules, additionalRules, concatArrays);
+    } catch (err) {
+        myConsole.log('No additional rules present in provided templates folder', err);
+    }
 }
 
-myConsole.log('Using rules from: %s', rulesTplFile);
-
-var rules = {};
-try {
-    rules = require(rulesTplFile);
-} catch (err) {
-    myConsole.log('Failed to load rules from JSON file: ', err);
-    process.exit(1);
-}
-
-if (process.argv[5] && process.argv[5].match(/\.json$/)) {
-    menuTplFile = path.isAbsolute(process.argv[5]) ? process.argv[5] : path.join(process.cwd(), process.argv[5]);
-}
-
-myConsole.log('Using menu tpl from: %s', menuTplFile);
-
-var menuTpls = {};
-try {
-    menuTpls = require(menuTplFile);
-} catch (err) {
-    myConsole.log('Failed to load menu tpls from JSON file: ', err);
-    process.exit(1);
-}
-
-var obj = new GitWikiToHTML({
+const obj = new GitWikiToHTML({
     'srcDir': process.argv[2] || null,
     'destDir': process.argv[3] || null,
     'rules': rules,
